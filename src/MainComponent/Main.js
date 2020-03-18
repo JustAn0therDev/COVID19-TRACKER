ef0470fd854fae35d19a219b5fc81f99039a607b
Ruan Montelo <ruanmontelo@gmail.com>
2020-03-17 21:15:44 -0300

    Installing axios and retrieving information about the Covid19Stats and cases

--- src/MainComponent/Main.js
+++ src/MainComponent/Main.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import './Main.css';
 import MainCovid19Data from "./MainCovid19DataComponent/MainCovid19Data";
 
 export default function Main() {
+    const [countryInputValue, setCountryInputValue] = useState('');
+    const [country, setCountry] = useState('');
+
     return (
         <>
         <div className="main-div">
@@ -14,15 +17,16 @@ export default function Main() {
                     <input 
                     id="input-search"
                     placeholder="Leave it blank to search for the total of Coronavirus cases"
+                    onChange={(ev) => { ev.preventDefault(); setCountryInputValue(ev.target.value); }}
                     />
                 </div>
-                <button id="btn-search" type="button">
+                <button id="btn-search" type="button" onClick={() => { setCountry(countryInputValue) }}>
                     Search!
                 </button>
              </form>
         </div>
         <br />
-        <MainCovid19Data></MainCovid19Data>
+        <MainCovid19Data props={country}></MainCovid19Data>
         </>
     )
 }
--- src/MainComponent/MainCovid19DataComponent/MainCovid19Data.js
+++ src/MainComponent/MainCovid19DataComponent/MainCovid19Data.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './MainCovid19Data.css';
+import axios from "axios";
 
-export default function MainCovid19Data() {
+export default function MainCovid19Data(props) {
+
+    const [componentResponseState, setComponentResponseState] = useState('');
+
+    useEffect(() => async function getCovid19DataFromSpecifiedCountry(country) {
+        var response = await axios.get('https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats', {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
+	            "x-rapidapi-key": "4d05572d05msh8850743a12d5d73p17280fjsn86033a0fdb9a"
+            },
+            params: {
+                "country": country
+            }
+        });
+        setComponentResponseState(response.data);
+    }, [props.country, componentResponseState]);
+
     return (
         <div className="div-mainCovid19Data">
-            <h3>Pais: Brasil</h3>
-            <br />
             <ul>
-                <li>Provincia: Nao identificada</li>
-                <li>Ultima atualizacao: {new Date().toLocaleDateString('pt-BR')}</li>
-                <li>Casos Confirmados: 10</li>
-                <li>Mortes: 1</li>
-                <li>Recuperados: 9</li>
-                <br />
-                <li>Ultima verificacao (amanha): {new Date().toLocaleDateString('pt-BR')}</li>
+            { componentResponseState.covid19Stats ? componentResponseState.covid19Stats.map(data =>(
+                <>
+                    <li>Provincia: {data.province}</li>
+                    <li>Ultima atualizacao: {data.lastUpdate.toLocaleDateString('pt-BR')}</li>
+                    <li>Casos Confirmados: {data.confirmed}</li>
+                    <li>Mortes: {data.deaths}</li>
+                    <li>Recuperados: {data.recovered}</li>
+                    <br />
+                </>
+                )
+            ) : <p>Search a country!</p> }
+                <li>Ultima verificacao: n tem</li>
                 <li>Total de Casos confirmados: 10</li>
                 <li>Total de Mortes: 1</li>
                 <li>Total de Recuperados: 9</li>
