901fb2b527300842f0242ebea430c8bc878ec909
Ruan Montelo <ruanmontelo@gmail.com>
2020-09-13 02:15:06 -0300

    Refactoring a bit of the HTML for accessibility, changing and using the already written button CSS and adding one more state variable to change only the main component state, without affecting the child component

--- src/MainComponent/Main.js
+++ src/MainComponent/Main.js
@@ -3,42 +3,48 @@ import "./Main.css";
 import MainCovid19Data from "./MainCovid19DataComponent/MainCovid19Data";
 
 export default function Main() {
-  const [countryToBeSearched, setCountryToBeSearched] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [countryToBeSearched, setCountryToBeSearchedThenAlertChildComponent] = useState("");
 
-  async function checkValueAndEnterKey(ev) {
-    if (ev.key === "Enter")
-      setCountryToBeSearched(await ev.target.value.replace(/\W/g, ""));
+  function setCountry(ev) {
+      setInputValue(ev.target.value.replace(/\W/g, ""));
+  }
+
+  async function lookupCountry() {
+      setCountryToBeSearchedThenAlertChildComponent(inputValue);
   }
 
   return (
     <>
       <div className="main-div">
         <div className="div-label-and-input">
-          <h1>COVID-19 | REPORTS</h1>
+          <h1>COVID-19: Cases</h1>
           <br />
-          <label>
-            Insert a country name and press enter (you can do that on your phone
-            too)!
-          </label>
-          <small>
-            <span className="underline">Things to keep in mind:</span>
-            <ul>
-              <li>
-                If data for the specified country is not found, the sum of all
-                the world's cases will be returned.
-              </li>
-              <li>This app is in english, so the name of the country must be as well.</li>
-              <li>And if this country has five or less provinces/states listed, I'll list them here for you!</li>
-            </ul>
-          </small>
-          <input
-            id="input-search"
-            placeholder="Insert a country name here!"
-            autoComplete="off"
-            onKeyPress={ev => {
-              checkValueAndEnterKey(ev);
-            }}
-          />
+          <form>
+            <fieldset>
+              <label>
+                Insert a country name and press enter (you can do that on your phone
+                too).
+              </label>
+              <small>
+                <ul>
+                  <li>
+                    Keep in mind that if data for the specified country is not found, the sum of all
+                    the world's cases will be returned.
+                  </li>
+                </ul>
+              </small>
+              <input
+                id="input-search"
+                placeholder="Insert a country name here!"
+                autoComplete="off"
+                onKeyPress={ev => {
+                  setCountry(ev);
+                }}
+              />
+            </fieldset>
+            <button type="button" onClick={() => { lookupCountry() }}>Search</button>
+          </form>
         </div>
       </div>
       <br />
